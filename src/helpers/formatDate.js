8dbd123bf5fcefd820333e24dfce0d74ec9cde4d
yuutaaaaaa <flydiflexer@gmail.com>
2024-07-27 16:49:34 +0300

    git init

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,12 @@
+
+
+
+export const formatDate = (date) => {
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return date.toLocaleDateString('en-US',options)
+}
