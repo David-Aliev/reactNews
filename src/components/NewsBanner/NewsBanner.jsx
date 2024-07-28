c9c3e15359ab8473375cb0a6af138c0a022cb321
David-Aliev <davidaliev664@gmail.com>
2024-07-28 13:11:08 +0300

    feat:(new section):added a news section below the main news image

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -9,8 +9,8 @@ export default function NewsBanner({item}) {
   return (
     <div className={styles.banner}>
         <Image image={item?.image} />
-        {/* <h3 className={styles.title}>{item.title}</h3>
-        <p className={styles.extra}>{formatTimeAgo(item.published)} by {item.author}</p> */}
+        <h3 className={styles.title}>{item.title}</h3>
+        <p className={styles.extra}>{formatTimeAgo(item.published)} by {item.author}</p>
     </div>
   )
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,27 +1,34 @@
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import NewsBanner from '../../components/NewsBanner/NewsBanner'
 import styles from './styles.module.css'
 import { getNews } from '../../API/apiNews'
+import NewsList from '../../components/NewsList/NewsList'
 
 
 const Main = () => {
 
+    const [news,setNews] = useState([]);
+
     useEffect(() => {
         const fetchNews = async() => {
             try {
-                const news = await getNews();
-                console.log(news)
+                const response = await getNews();
+                console.log(response)
+                setNews(response.news)
             } catch (error) {
                 console.log(error)
             }
         }
         fetchNews()
-    },[])
+    },[]);
+
+  
 
     return (
         <main className={styles.main}>
-            <NewsBanner />
+            {news.length > 0 ? <NewsBanner item={news[0]}/> : null}
+            <NewsList news={news} />
         </main>
     )
 }
